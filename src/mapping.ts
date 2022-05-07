adbc9c8f01ce44949c8564ee4c6706ca0bd51293
Justin <justintorre75@gmail.com>
2022-05-06 21:56:23 -0700

    MVP

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,31 +5,29 @@ import {
   OwnerChanged,
   SellOrderCreated
 } from "../generated/OrderBook/OrderBook"
-import { ExampleEntity } from "../generated/schema"
+import { SellOrder } from "../generated/schema"
 
 export function handleFeeChanged(event: FeeChanged): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity.previous = event.params.previous
-  entity.next = event.params.next
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  // entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -54,4 +52,19 @@ export function handleFeeChanged(event: FeeChanged): void {
 
 export function handleOwnerChanged(event: OwnerChanged): void {}
 
-export function handleSellOrderCreated(event: SellOrderCreated): void {}
+export function handleSellOrderCreated(event: SellOrderCreated): void {
+  let entity = SellOrder.load(event.transaction.from.toHex())
+  if (!entity) {
+    entity = new SellOrder(event.transaction.from.toHex())
+
+    // Entity fields can be set using simple assignments
+  }
+  // BigInt and BigDecimal math are supported
+  entity.address = event.params.sellOrder;
+
+
+
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
